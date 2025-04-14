761f9d6b0b853a4e93497ea003b115a7cf57f1ef
Pedro Henrique <pedrohenrique2937@gmail.com>
2025-04-14 17:33:27 -0300

    fileCloudSaving

--- api/src/main/java/com/eventostec/api/service/EventService.java
+++ api/src/main/java/com/eventostec/api/service/EventService.java
@@ -1,16 +1,30 @@
 package com.eventostec.api.service;
 
+import com.amazonaws.services.s3.AmazonS3;
 import com.eventostec.api.domain.event.Event;
 import com.eventostec.api.domain.event.EventRequestDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Date;
+import java.util.Objects;
+import java.util.UUID;
 
 @Service
 public class EventService {
+
+    @Value("${aws.bucket.name}")
+    private String bucketName;
+    @Autowired
+    private AmazonS3 s3Client;
     public Event createEvent(EventRequestDTO data){
             String imgUrl = null;
+
 
             if(data.image() != null){
                 imgUrl = this.uploadImg(data.image());
@@ -29,6 +43,23 @@ public class EventService {
 
     }
     private String uploadImg(MultipartFile img){
-        return "";
+        String imgName = UUID.randomUUID() + "-" + img.getOriginalFilename();
+
+        try{
+            File file = this.convertToMultipartFile(img);
+            s3Client.putObject(bucketName,imgName,file);
+            file.delete();
+            return s3Client.getUrl(bucketName,imgName).toString();
+        } catch(Exception e){
+            System.out.println("Erro ao subir aquivo");
+            return null;
+        }
+    }
+    private File convertToMultipartFile(MultipartFile multipartFile) throws IOException {
+        File file = new File(Objects.requireNonNull(multipartFile.getOriginalFilename()));
+        FileOutputStream fos = new FileOutputStream(file);
+        fos.write(multipartFile.getBytes());
+        fos.close();
+        return file;
     }
 }
